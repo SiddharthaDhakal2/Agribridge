dfe71839ea0153ecf64a0174e8a71ec7d553d4fe
Siddhartha228 <152017010+Siddhartha228@users.noreply.github.com>
2025-12-18 16:04:37 +0545

    Add bottom navigation

---
+++ lib/screens/button_navigation.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+import 'cart_screen.dart';
+import 'order_screen.dart';
+import 'profile_screen.dart';
+
+class ButtonNavigation extends StatefulWidget {
+  const ButtonNavigation({Key? key}) : super(key: key);
+
+  @override
+  State<ButtonNavigation> createState() => _ButtonNavigationState();
+}
+
+class _ButtonNavigationState extends State<ButtonNavigation> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    CartScreen(),
+    OrderScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        backgroundColor: Colors.white,
+
+        selectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
