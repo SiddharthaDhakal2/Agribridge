2a3c9f4577089ea568d78c6f41f1eee3b9c7964e
Siddhartha228 <152017010+Siddhartha228@users.noreply.github.com>
2026-01-14 21:25:57 +0545

    Add auth hive model

---
+++ lib/features/auth/data/models/auth_hive_model.dart
@@ -0,0 +1,56 @@
+import 'package:hive/hive.dart';
+import 'package:agribridge/core/constants/hive_table_constants.dart';
+import 'package:agribridge/features/auth/domain/entities/auth_entity.dart';
+import 'package:uuid/uuid.dart';
+
+part 'auth_hive_model.g.dart';
+
+@HiveType(typeId:HiveTableConstant.authTypeId)
+class AuthHiveModel extends HiveObject{
+
+  @HiveField(0)
+  final String? authId;
+  @HiveField(1)
+  final String fullName;
+  @HiveField(2)
+  final String email;
+  @HiveField(3)
+  final String username;
+  @HiveField(4)
+  final String? password;
+  @HiveField(5)
+  final String? profilePicture;
+
+  AuthHiveModel({
+    String? authId,
+    required this.fullName,
+    required this.email,
+    required this.username,
+    this.password,
+    this.profilePicture,
+  }) : authId = authId ?? Uuid().v4();
+
+  //From Entity
+   factory AuthHiveModel.fromEntiity(AuthEntity entity){
+    return AuthHiveModel(
+      authId: entity.authId,
+      fullName: entity.fullName,
+      email: entity.email,
+      username: entity.username,
+      password: entity.password,
+      profilePicture: entity.profilePicture,
+    );
+   }
+
+  //To Entity
+  AuthEntity toEntity(){
+    return AuthEntity(
+      authId: authId,
+      fullName: fullName,
+      email: email,
+      username: username,
+      password: password,
+      profilePicture: profilePicture,
+    );
+  }
+}
