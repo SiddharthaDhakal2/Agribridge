68c3d5b22594004c316645a6c0a66cfb0fe01bb9
Siddhartha228 <152017010+Siddhartha228@users.noreply.github.com>
2026-01-17 21:38:03 +0545

    update

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -1,3 +1,5 @@
+// import 'package:agribridge/features/auth/data/models/auth_api_model.dart';
+import 'package:agribridge/features/auth/data/models/auth_api_model.dart' show AuthApiModel;
 import 'package:agribridge/features/auth/data/models/auth_hive_model.dart';
 
 abstract interface class IAuthDatasource{
@@ -10,3 +12,26 @@ abstract interface class IAuthDatasource{
   Future<bool> isEmailExists(String email);
 }
 
+abstract interface class IAuthRemoteDataSource {
+  Future<AuthApiModel> register(AuthApiModel user);
+  Future<AuthApiModel?> login(String email, String password);
+  Future<AuthApiModel?> getUserById(String authId);
+}
+
+
+
+// abstract interface class IAuthLocalDataSource {
+//   Future<AuthHiveModel> register(AuthHiveModel user);
+//   Future<AuthHiveModel?> login(String email, String password);
+//   Future<AuthHiveModel?> getCurrentUser();
+//   Future<bool> logout();
+//   //get email existence
+//   Future<AuthHiveModel?> getUserByEmail(String email);
+// }
+
+// abstract interface class IAuthRemoteDataSource {
+//   Future<AuthApiModel> register(AuthApiModel user);
+//   Future<AuthApiModel?> login(String email, String password);
+//   Future<AuthApiModel?> getUserById(String authId);
+  
+// }
