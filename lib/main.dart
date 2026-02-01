2553924f001e029ff331a565b6c67d59a46c7221
Siddhartha228 <152017010+Siddhartha228@users.noreply.github.com>
2026-02-01 17:10:56 +0545

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:agribridge/app/app.dart';
 import 'package:agribridge/core/services/hive/hive_service.dart';
+import 'package:hive/hive.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:agribridge/features/dashboard/data/models/profile_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
+  // Register the adapter before opening boxes
+  Hive.registerAdapter(ProfileModelAdapter()); 
+
   // Initialize Hive
   final hiveService = HiveService();
   await hiveService.init();
-  await hiveService.openBoxes(); 
+  await hiveService.openBoxes();
 
   // shared pref
   final sharedPrefs = await SharedPreferences.getInstance();
-  
+
   runApp(
     ProviderScope(
       overrides: [sharedPreferencesProvider.overrideWithValue(sharedPrefs)],
