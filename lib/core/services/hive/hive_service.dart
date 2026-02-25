15077e8ad9b732dc77f1490e9a6e38bcacabea0d
Siddhartha228 <152017010+Siddhartha228@users.noreply.github.com>
2026-02-25 21:39:33 +0545

    Update HiveService logic and methods

--- lib/core/services/hive/hive_service.dart
+++ lib/core/services/hive/hive_service.dart
@@ -1,8 +1,8 @@
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 import 'package:agribridge/core/constants/hive_table_constants.dart';
 import 'package:agribridge/features/auth/data/models/auth_hive_model.dart';
+import 'package:agribridge/features/dashboard/data/models/home_hive_model.dart';
 import 'package:path_provider/path_provider.dart';
 
 final hiveServiceProvider = Provider<HiveService>((ref){
@@ -10,22 +10,28 @@ final hiveServiceProvider = Provider<HiveService>((ref){
 });
 class HiveService {
 
-//init
+  //init
   Future<void> init() async{
     final directory = await getApplicationDocumentsDirectory();
     final path  = '${directory.path}/${HiveTableConstant.dbName}';
     Hive.init(path);
     _registerAdapter();
+    await openBoxes();
   }
-//Register Adapters
+  //Register Adapters
   void _registerAdapter(){
     if(!Hive.isAdapterRegistered(HiveTableConstant.authTypeId)){
       Hive.registerAdapter(AuthHiveModelAdapter());
     }
+    // Register HomeHiveModelAdapter for products
+    if(!Hive.isAdapterRegistered(HiveTableConstant.productTypeId)){
+      Hive.registerAdapter(HomeHiveModelAdapter());
+    }
   }
-//Open Boxes
+  //Open Boxes
   Future<void> openBoxes() async{
     await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTable);
+    await Hive.openBox<HomeHiveModel>(HiveTableConstant.productTable);
   }
 //Close Boxes
   Future<void> close() async{
