e1cafc86ee59822d0f1996edf98888bf3904985a
Siddhartha228 <152017010+Siddhartha228@users.noreply.github.com>
2026-02-25 21:33:03 +0545

    Add generated Hive adapter for HomeHiveModel

---
+++ lib/features/dashboard/data/models/home_hive_model.g.dart
@@ -0,0 +1,65 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_hive_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class HomeHiveModelAdapter extends TypeAdapter<HomeHiveModel> {
+  @override
+  final int typeId = 2;
+
+  @override
+  HomeHiveModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return HomeHiveModel(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      price: fields[2] as double,
+      quantity: fields[3] as int,
+      image: fields[4] as String,
+      category: fields[5] as String,
+      description: fields[6] as String,
+      unit: fields[7] as String,
+      availability: fields[8] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, HomeHiveModel obj) {
+    writer
+      ..writeByte(9)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.price)
+      ..writeByte(3)
+      ..write(obj.quantity)
+      ..writeByte(4)
+      ..write(obj.image)
+      ..writeByte(5)
+      ..write(obj.category)
+      ..writeByte(6)
+      ..write(obj.description)
+      ..writeByte(7)
+      ..write(obj.unit)
+      ..writeByte(8)
+      ..write(obj.availability);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HomeHiveModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
