0b21b51e0b7f1490440da6e3b4c344d10c9bcaf7
Siddhartha228 <152017010+Siddhartha228@users.noreply.github.com>
2026-02-25 21:34:37 +0545

    Add HomeEntity for dashboard domain

---
+++ lib/features/dashboard/domain/entities/home_entity.dart
@@ -0,0 +1,23 @@
+class HomeEntity {
+	final String id;
+	final String name;
+	final double price;
+	final int quantity;
+	final String image;
+	final String category;
+	final String description;
+	final String unit;
+	final String availability;
+
+	HomeEntity({
+		required this.id,
+		required this.name,
+		required this.price,
+		required this.quantity,
+		required this.image,
+		required this.category,
+		required this.description,
+		required this.unit,
+		required this.availability,
+	});
+}
