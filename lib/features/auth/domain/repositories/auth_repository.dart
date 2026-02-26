58b00d40761f67b4e470fe338618f200b94bfe75
Siddhartha228 <152017010+Siddhartha228@users.noreply.github.com>
2026-02-26 23:57:18 +0545

    add change password contract in auth repository

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -2,9 +2,14 @@ import 'package:dartz/dartz.dart';
 import 'package:agribridge/core/error/failures.dart';
 import 'package:agribridge/features/auth/domain/entities/auth_entity.dart';
 
-abstract interface class IAuthRepository{
+abstract interface class IAuthRepository {
   Future<Either<Failure, bool>> register(AuthEntity entity);
   Future<Either<Failure, AuthEntity>> login(String email, String password);
   Future<Either<Failure, AuthEntity>> getCurrentUser();
   Future<Either<Failure, bool>> logout();
+  Future<Either<Failure, bool>> changePassword(
+    String userId,
+    String currentPassword,
+    String newPassword,
+  );
 }
