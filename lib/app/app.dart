99a253eae64a650224a10da81b38ab4e98fab7e2
Siddhartha228 <152017010+Siddhartha228@users.noreply.github.com>
2026-02-27 09:47:13 +0545

    wire app root to light/dark theme mode

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,23 @@
 //import 'package:agribridge/screens/login_screen.dart';
 import 'package:agribridge/features/splash/presentation/pages/splash_screen.dart';
+import 'package:agribridge/app/theme/theme_data.dart';
+import 'package:agribridge/app/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
+
     return MaterialApp(
+      theme: getApplicationTheme(),
+      darkTheme: getApplicationDarkTheme(),
+      themeMode: themeMode,
       //home:LoginScreen(),
-      home:SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
